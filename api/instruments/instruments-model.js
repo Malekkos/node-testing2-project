5dafd16e486d7381a38c25b392a746cfe78970a4
Christian Anderson <tehninjawaffle@gmail.com>
2024-01-19 11:46:41 -0800

    Setup model

---
+++ api/instruments/instruments-model.js
@@ -0,0 +1,21 @@
+const db = require("../../data/db-config")
+
+function getAll() {
+  return db("instruments")
+}
+
+function getById(id) {
+  return db("instruments")
+  .where("id", id)
+  .first()
+}
+
+function insert(instrument) {
+  return db("instruments")
+  .insert(instrument)
+  .then(([id]) => {
+    return db("hobbits").where("id", id)
+  })
+}
+
+module.exports = { getAll, getById, insert }
