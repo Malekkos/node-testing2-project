d0dcfc73ed5af4be27c1ba03c7b95feee9ecb626
Christian Anderson <tehninjawaffle@gmail.com>
2024-01-19 13:01:40 -0800

    Added two tests for getAll and getById

--- api/instruments/instruments-model.test.js
+++ api/instruments/instruments-model.test.js
@@ -24,3 +24,27 @@ describe("Everything is right in the universe", () => {
     expect(string).toContain("r")
   })
 })
+
+describe("getAll method works as intended", () => {
+  test("The return of getAll has a length of 8", async () => {
+    const result = await Instruments.getAll()
+    expect(result.length).toEqual(8)
+  })
+  test("the return of a instrument from getAll has an instrument_name and instrument_family", async () => {
+    const result = await Instruments.getAll()
+    expect(result[3].instrument_name).toBe("Clarinet")
+    expect(result[3].instrument_family).toBe("Woodwind")
+  })
+})
+
+describe("getById method works as intended", () => {
+  test("the return of getById is only one object", async () => {
+    const result = await Instruments.getById(3)
+    const expected = {"instrument_family": "Woodwind", "instrument_id": 3, "instrument_name": "Flute"}
+    expect(result).toMatchObject(expected)
+  })
+  test("the return of getById has 3 fields", async () => {
+    const result = await Instruments.getById(6)
+    expect(result).toHaveProperty("instrument_family" && "instrument_name" && "instrument_id")
+  })
+})
