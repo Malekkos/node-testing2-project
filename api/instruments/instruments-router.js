552f8c38b6c9064d4f0e04fe39594ff98df38172
Christian Anderson <tehninjawaffle@gmail.com>
2024-01-19 12:20:39 -0800

    Added findById endpoint and post endpoint

--- api/instruments/instruments-router.js
+++ api/instruments/instruments-router.js
@@ -6,5 +6,12 @@ router.get("/", (req, res, next) => {
   res.json("WIP")
 })
 
+router.get("/:id", (req, res, next) => {
+  res.json("WIP 2")
+})
+
+router.post("/", (req, res, next) => {
+  res.json("WIP 3")
+})
 
 module.exports = router
