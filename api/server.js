713d19e01a71b461676b1b1f1d119dc04c6a4dc0
Christian Anderson <tehninjawaffle@gmail.com>
2024-01-19 13:49:09 -0800

    That was the issue, moved the endpoints over and deleted the now unused file

--- api/server.js
+++ api/server.js
@@ -1,10 +1,29 @@
 const express = require("express")
 const server = express()
-const instrumentsRouter = require("./instruments/instruments-router")
 
 server.use(express.json())
 
-server.use("/api/instruments", instrumentsRouter)
+const Instruments = require("./instruments/instruments-model")
+
+
+server.get("/", (req, res, next) => {
+  Instruments.getAll()
+  .then(instruments => {
+    res.status(200).json(instruments)
+  })
+  .catch(error => {
+    next(error)
+  })
+})
+
+server.get("/:id", (req, res, next) => {
+  res.json("WIP 2")
+})
+
+server.post("/", (req, res, next) => {
+  res.json("WIP 3")
+})
+
 
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
